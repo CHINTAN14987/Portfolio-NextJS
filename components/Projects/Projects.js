bc44565b46159fc5d34cab4456770266a8ab856d
Chintan14987 <chintan14987@gmail.com>
2022-12-05 12:56:48 +0530

    refactor

--- components/Projects/Projects.js
+++ components/Projects/Projects.js
@@ -1,38 +1,53 @@
-
-import React, { useEffect } from 'react'
-import data from './data'
-import styles from "./Projects.module.css"
+import React, { useEffect } from "react";
+import data from "./data";
+import styles from "./Projects.module.css";
 
 const Projects = () => {
-    useEffect(()=>{ 
-      import("@lottiefiles/lottie-player");
-},[])
+  useEffect(() => {
+    import("@lottiefiles/lottie-player");
+  }, []);
   return (
     <div className={styles.projectMainContainer}>
-     <h2 className={styles.heading}>Few Professional Projects made for Product Clients</h2>
-    <div className={styles.container }>  {data.slice(0,3).map((item, index)=>{return (<div key={index} className={styles.card }>
-    <div className={  styles.cardWrapper}>    
-    <div className={styles.lottieWarpper}><h3>{item.Project}</h3>
-    <lottie-player
-  autoplay
-  loop
-  mode="normal"
-  src={item.link}
-style={{width:"180px"}}
->
-</lottie-player>
+      <h2 className={styles.heading}>
+        Few Professional Projects made for Product Clients
+      </h2>
+      <div className={styles.container}>
+        {data.map((item, index) => {
+          return (
+            <div key={index} className={styles.card}>
+              <div className={styles.cardWrapper}>
+                <div className={styles.lottieWarpper}>
+                  <h3>{item.Project}</h3>
+                  <lottie-player
+                    autoplay
+                    loop
+                    mode="normal"
+                    src={item.link}
+                    style={{ width: "200px" }}
+                  ></lottie-player>
+                </div>
+
+                <p>
+                  {" "}
+                  <strong>Description:-</strong> {item.description}
+                </p>
+
+                <p>
+                  <strong>Technologies:-</strong> {item.techStack}
+                </p>
+              </div>
+              {
+                <p>
+                  {" "}
+                  <strong>Responsibilities:-</strong> {item.responsibilities}
+                </p>
+              }
+            </div>
+          );
+        })}
+      </div>
     </div>
-  
-        <p> <strong>Description:-</strong> {item.description}</p>
-   
-        <p><strong>Technologies:-</strong> {item.techStack}</p></div>
-        {<p> <strong>Responsibilities:-</strong> {item.responsibilities}</p>}
+  );
+};
 
-    </div>)})}</div>
-    
-    </div>
-  )
-
-}
-
-export default Projects
+export default Projects;
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,17 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Header from '../components/Header/Header'
-import Hero from '../components/Hero/Hero'
-import Projects from '../components/Projects/Projects'
-import Technologies from '../components/Technologies/Technologies'
-
+import Head from "next/head";
+import Image from "next/image";
+import Header from "../components/Header/Header";
+import Hero from "../components/Hero/Hero";
+import Projects from "../components/Projects/Projects";
+import Technologies from "../components/Technologies/Technologies";
 
 export default function Home() {
   return (
-  <><Header/>
-  <Hero/>
-  <Projects/>
-  <Technologies/>
-  </>
-  )
+    <div style={{ width: "90%", margin: "auto" }}>
+      <Header />
+      <Hero />
+      <Projects />
+      <Technologies />
+    </div>
+  );
 }
